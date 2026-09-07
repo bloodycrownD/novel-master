365959cbde8b7ebc1ffb29728ea03cdb896692b6
BloodyCrown <3170989534@qq.com>
2026-09-08 00:39:10 +0800

    Revert "feat(mobile): 沉淀救援模式能力（RESCUE_EXPORT_MODE，默认关闭）"
    This reverts commit 8c032db491dd30c16e7643fb061f1aeecb501ad2.

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -10,8 +10,6 @@ import {ThemeProvider, useTheme} from './theme/ThemeProvider';
 import {ToastHost} from './components/chrome/ToastHost';
 import {UpdateCheckHost} from './components/update/UpdateCheckHost';
 import {RootNavigator} from './navigation/RootNavigator';
-import {RESCUE_EXPORT_MODE} from './rescue/rescue-mode';
-import {RescueExportScreen} from './screens/rescue/RescueExportScreen';
 
 function AppContent() {
   const {mode} = useTheme();
@@ -35,13 +33,7 @@ function App() {
       <KeyboardProvider preload={false}>
         <NovelMasterProvider>
           <ThemeProvider>
-            {/* 救援模式：跳过全部常规 UI（含崩溃链所在的会话页与更新检查），
-                仅提供数据库导出；见 rescue/rescue-mode.ts 的使用守则 */}
-            {RESCUE_EXPORT_MODE ? (
-              <RescueExportScreen />
-            ) : (
-              <AppContent />
-            )}
+            <AppContent />
           </ThemeProvider>
         </NovelMasterProvider>
       </KeyboardProvider>
